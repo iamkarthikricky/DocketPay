62fd781900e1cbcd588e942dc10c1ac50be46f52
iamkarthikricky <iamkarthikricky@gmail.com>
2024-12-24 18:51:19 +0530

    updated UI-one

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './global.css'
-import LoginPage from "./components/LoginPage/loginPage"
+import LoginPage from './components/LoginPage/loginPage';
 import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom';
-import CreateAccount from './components/LoginPage/createAccount';
 import ProtectedRoute from './protectedRoute';
 import HomePage from './components/HomePage/homePage';
 import ForgotPassword from './components/LoginPage/forgotPassword';
-
+import CreateAccount from './components/LoginPage/signup';
+import {ToastContainer} from 'react-toastify';
 const App=()=>{
 
   const token = localStorage.getItem('token');  // Check if the token exists in localStorage
 
   return(
+    <>
     <Router>
        <Routes>
           {/* Redirect to /login if the user is not logged in, otherwise go to /home */}
@@ -20,16 +21,21 @@ const App=()=>{
           <Route path="/login" element={token ? <Navigate to="/home" replace /> : <LoginPage />} />
 
           {/* If user is logged in, redirect them to /home, else show the create account page */}
-          <Route path="/create-account" element={token ? <Navigate to="/home" replace /> : <CreateAccount />} />
+          <Route path="/signup" element={token ? <Navigate to="/home" replace /> : <CreateAccount />} />
 
           
           {/* If user is logged in, redirect them to /home, else show the create account page */}
           <Route path="/forgot-password" element={token ? <Navigate to="/home" replace /> : <ForgotPassword />} />
 
+             {/* If user is logged in, redirect them to /home, else show the create account page */}
+             <Route path="/verify-account" element={token ? <Navigate to="/home" replace /> : <ForgotPassword />} />
+
           {/* Protected route for /home */}
           <Route path="/home" element={<ProtectedRoute element={HomePage} />} />
         </Routes>
   </Router>
+  <ToastContainer />
+  </>
   )
 }
 
--- src/components/LoginPage/loginPage.jsx
+++ src/components/LoginPage/loginPage.jsx
@@ -1,17 +1,17 @@
 import styles from "./loginPage.module.css";
 
-import { useState } from "react";
-import { FaArrowLeftLong } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
 
-import { Divider, Form, Input } from "antd";
+import { Button, Divider, Form, Input,Alert } from "antd";
 
+import { useState } from "react";
 import styled from "styled-components";
 
-
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import BrandImg from "../../assets/brand_img.png";
 import axiosInstance from "../../axiosConfig/axiosConfig";
+
+import { toast } from 'react-toastify';
 
 // Create a styled component for Input
 
@@ -20,29 +20,23 @@ import axiosInstance from "../../axiosConfig/axiosConfig";
 export const StyledForm = styled(Form)`
   display: flex;
   flex-direction: column;
+  gap:0px !important;
 `;
 
 export const StyledFormItem = styled(Form.Item)`
-  margin-bottom: 15px; // Custom spacing between form items
+    margin-bottom:15px !important;
   .ant-form-item-label{
-    padding:0px !important;
+    padding-bottom:3px !important;
+  }
+
+  .ant-form-item-label > label {
+    height:0px;
     font-family:var(--font-public);
-    font-size:var(--sub-heading)
+    font-size:var(--sub-heading);
+    font-weight:var(--font-weight500);
+    color:var(--color-black);
     text-align:left !important;
-  }
-
-    .ant-form-item-required::after{
-      display:none !important;
-  }
-       .ant-form-item-required::before{
-      display:none !important;
-  }
-    .required-star {
-      margin-left: 4px;
-      color: red; // Customize * color
-    }
-  }
-
+}
   .ant-form-item-explain-error{
     font-size:var(--main-para);
     font-family:var(--font-public);
@@ -52,10 +46,10 @@ export const StyledFormItem = styled(Form.Item)`
 
 export const StyledFormInput = styled(Input)`
   width: 100%; // Custom width
-  border-radius: 8px; // Rounded corners
+  border-radius: 5px; // Rounded corners
   border: 1px solid #c9c9c9; // Green border
   background-color: white; // Light background color
-  height: 40px;
+  height:35px;
   font-family: var(--font-public);
   font-size: var(--main-para);
 
@@ -77,10 +71,10 @@ export const StyledFormInput = styled(Input)`
 // Styled Input.Password
 export const StyledPasswordInput = styled(Input.Password)`
   width: 100%; // Custom width
-  border-radius: 8px; // Rounded corners
+  border-radius: 5px; // Rounded corners
   border: 1px solid #c9c9c9; // Green border
   background-color: white; // Light background color
-  height: 40px;
+  height: 35px;
   font-family: var(--font-public);
   font-size: var(--main-para);
 
@@ -120,221 +114,182 @@ export const StyledPasswordInput = styled(Input.Password)`
   }
 `;
 
-export const EmailLogin = ({ onCancel }) => {
-  const [form] = Form.useForm();
 
-  const validatePassword = (_, value) => {
-   
+export const StyledButton = styled(Button)`
+width:100%;
+height:36px;
+margin-top:20px;
+font-family:var(--font-public);
+font-size:var(--sub-heading);
+color:#ffffff;
+  background-color: #000000 !important;
+  color: white !important;
+  border: none !important;
+  &:hover,
+  &:focus {
+    background-color: #000000 !important;
+    color: white !important;
+  }
+  &.ant-btn-loading {
+    background-color: #000000 !important;
+    color: white !important;
+  }
+`;
 
-    if (!value) {
-      return Promise.reject(new Error("Password is required."));
-    }
-    return Promise.resolve();
-  };
+export const EmailLogin = () => {
+    const [form] = Form.useForm();
 
-  const validateEmail = (_, value) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!value) {
-      return Promise.reject(new Error("Email is required"));
-    } else if (!emailRegex.test(value)) {
-      return Promise.reject(new Error("Please enter a valid email address"));
-    }
-    return Promise.resolve();
-  };
-
-  const onFinish = async (values) => {
-    const { email, password } = values;
-    const requestData = { email, password };
-
-    try {
-      const response = await axiosInstance.post("/login", requestData);
-
-      if (response.status === 200) {
-          localStorage.setItem("token",response.data.token)
+    const [isLoading,setIsLoading] = useState(false)
+  
+    const validatePassword = (_, value) => {
+      if (!value) {
+        return Promise.reject(new Error("Password is required."));
       }
-    } catch (error) {
-      if (error.response) {
-        const { status, data } = error.response;
-        const errorMessage = data?.errorMessage || "An error occurred";
+      return Promise.resolve();
+    };
+  
+    const validateEmail = (_, value) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!value) {
+        return Promise.reject(new Error("Email is required"));
+      } else if (!emailRegex.test(value)) {
+        return Promise.reject(new Error("Please enter a valid email address"));
+      }
+      return Promise.resolve();
+    };
+  
+    const navigate=useNavigate()
 
-        // Define a common function to set error for form fields
-        const setFieldError = (fieldName, message) => {
-          form.setFields([
-            {
-              name: fieldName,
-              errors: [message],
-            },
-          ]);
-        };
-
-        // Handle different status codes and set appropriate field errors
-        if (status === 404) {
-          setFieldError("email", errorMessage);
-        } else if (status === 400) {
-          setFieldError("password", errorMessage);
+    const onFinish = async (values) => {
+      const { email, password } = values;
+      const requestData = { email, password };  
+      try {
+        setIsLoading(true)
+        const response = await axiosInstance.post("/sign-in", requestData);
+        if (response.status === 200) {
+            setIsLoading(false)
+            localStorage.setItem("token",response.data.token)
+            navigate("/home")
+        }
+        else{
+          alert("Something went wrong !")
+        }
+      } catch (error) {
+        setIsLoading(false)
+        if (error.response) {
+          const { status, data } = error.response;
+          const errorMessage = data?.errorMessage || "An error occurred";
+          // Define a common function to set error for form fields
+          const setFieldError = (fieldName, message) => {
+            form.setFields([
+              {
+                name: fieldName,
+                errors: [message],
+              },
+            ]);
+          };
+  
+          // Handle different status codes and set appropriate field errors
+          if (status === 404) {
+            setFieldError("email", errorMessage);
+          } else if (status === 400) {
+            setFieldError("password", errorMessage);
+          }
+        
+        }
+        else{
+          toast.error("Something went wrong.")
         }
       }
-    }
-  };
-
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
-
-  return (
-    <div className="bg-white h-full p-4 md:p-0 sm:p-4">
-      <div className="md:hidden">
-        <div className="flex w-2/3 justify-between align-middle">
-          <button onClick={onCancel}>
-            <FaArrowLeftLong fontSize={"16px"} />
-          </button>
-          <p className={`${styles.email_text} text-base`}>Login with Email</p>
-        </div>
-      </div>
-      <div className="pt-5 md:pt-0">
-        <StyledForm
-          form={form}
-          name="loginForm"
-          labelCol={{
-            span: 24,
-          }}
-          wrapperCol={{
-            span: 24,
-          }}
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-        >
-          <StyledFormItem
-            label={
-              <>
-                Email
-                <span className="required-star">*</span>
-              </>
-            }
-            name="email"
-            rules={[{ validator: validateEmail }]}
+    };
+  
+    const onFinishFailed = (errorInfo) => {
+      console.log("Failed:", errorInfo);
+    };
+  
+    return (
+      <div className="bg-white h-full mt-3">
+          <StyledForm
+            form={form}
+            name="loginForm"
+            labelCol={{
+              span: 24,
+            }}
+            wrapperCol={{
+              span: 24,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
           >
-            <StyledFormInput placeholder="Enter email address" />
-          </StyledFormItem>
-          <StyledFormItem
-            label={
-              <>
-                Password
-                <span className="required-star">*</span>
-              </>
-            }
-            name="password"
-            rules={[{ validator: validatePassword }]}
-          >
-            <StyledPasswordInput placeholder="Enter your password" />
-          </StyledFormItem>
-          <Link to="/forgot-password" className={styles.forgot_password}>
-            Forgot Password ?
-          </Link>
-          <StyledFormItem>
-            <button
-              className="w-full h-9 my-2 border rounded-lg text-white bg-blue-400 flex items-center justify-center gap-2"
-              type="submit"
+            <StyledFormItem
+                label="Email"
+              name="email"
+              rules={[{ validator: validateEmail }]}
             >
-              Login
-            </button>
-          </StyledFormItem>
-        </StyledForm>
-      </div>
-    </div>
-  );
-};
-
-const LoginPage = () => {
-  const [showLogin, setShowLogin] = useState(false);
-
-  return (
-    <div className="w-screen h-screen">
-      <div
-        className={`${styles.landing_page_container} relative container h-full md:hidden`}
-      >
-        {showLogin ? (
-          <EmailLogin onCancel={() => setShowLogin(false)} />
-        ) : (
-          <div className="h-full">
-            <div className="h-3/4 flex justify-center items-center">
-              <img src={BrandImg} alt="Brand Logo" className="w-4/5 h-auto" />
-            </div>
-
-            <div className="h-60 w-full absolute bottom-0 rounded-t-2xl px-8 py-6 bg-white flex flex-col gap-4">
-              <div className="w-full h-12 border rounded-lg flex items-center justify-center gap-2">
-                <FcGoogle />
-                <p className={styles.email_text}>Continue with Google</p>
-              </div>
-
-              <div className="w-full flex items-center justify-center">
-                <button
-                  className={styles.login_secondary_btn}
-                  onClick={() => setShowLogin(true)}
-                >
-                  Use other email ID
-                </button>
-              </div>
-              <p className={`${styles.form_text}`}>
-                Don't have an account ?{" "}
-                <NavLink to="/create-account"  className={styles.create_now_text}>
-                  Create one
-                </NavLink>
-              </p>
-            </div>
-          </div>
-        )}
-      </div>
-
-      <div className="hidden md:flex w-full h-4/5">
-
-        <div className="relative w-1/2 justify-start p-7 pr-0 border-b">
-          <div className="flex flex-col gap-2">
-            <h1 className={styles.login_heading}>Get Started Now</h1>
-            <p>Enter your credentials to access your account.</p>
-            <div className="flex gap-3 w-96">
-              <button className="w-1/2 h-9 border rounded-lg flex items-center justify-center gap-2 px-1">
-                <FcGoogle />
-                <p className={styles.email_text}>Continue with Google</p>
-              </button>
-              {/* <button className="w-1/2 h-9 border rounded-lg flex items-center justify-center gap-2">
-                <FaFacebook />
-                <p className={styles.email_text}>Login with Facebook</p>
-              </button> */}
-            </div>
-          </div>
-          <div className="w-4/5">
-                
+              <StyledFormInput disabled={isLoading} placeholder="Enter email address" />
+            </StyledFormItem>
+            <StyledFormItem
+            label="Password"
+              name="password"
+              rules={[{ validator: validatePassword }]}
+            >
+              <StyledPasswordInput  disabled={isLoading} placeholder="Enter your password" />
+            </StyledFormItem>
+            <Link to="/forgot-password" className={styles.forgot_password}>
+              Forgot Password ?
+            </Link>
+            <StyledFormItem>
+              <StyledButton loading={isLoading}
+              
+                htmlType="submit"
+              >
+               Login
+              </StyledButton>
+            </StyledFormItem>
+          </StyledForm>
           <Divider plain style={{
       fontSize: '12px',
       color: '#ccc',
       borderColor: '#c9c9c9',
-      
+      margin:"0px"
     }}>or</Divider>
-
-         
-          <EmailLogin onCancel={() => setShowLogin(false)} />
-
-          <p className={`${styles.form_text}`}>
-                Don't have an account ?{" "}
-                <NavLink to="/create-account"  className={styles.create_now_text}>
-                  Create one
+        <button className="w-full mt-5 h-9 border rounded-lg flex items-center justify-center gap-2 ">
+                <FcGoogle />
+                <p className={styles.social_text}>Continue with Google</p>
+              </button>
+              <p className={`${styles.description} text-center mt-5`}>
+                Don't have an account?{" "}
+                <NavLink to="/signup"  className={styles.create_now_text}>
+                  Register
                 </NavLink>
               </p>
-          </div>
-        </div>
+      </div>
+    );
+  };
 
-        <div className={`${styles.landing_page_container} h-full  w-1/2 flex justify-center items-center`}>
-              <img src={BrandImg} alt="Brand Logo" className="w-1/2" />
-            </div>
-        </div>
+
+
+
+const NewLoginPage = () => {
+  return (
+    <div className="w-screen h-screen">
+      <div className="w-full h-full p-5 md:p-0 md:w-1/2 border md:flex justify-center items-center">
+      <div>
+      <div className="flex flex-col md:max-w-96 justify-center items-center gap-2">
+        <img src={BrandImg} alt="brand-logo" className={`${styles.brand_img} text-center`}/>
+        <h1 className={styles.welcome_text}>Welcome Back to DocketPay</h1>
+        <p className={styles.description}>Enter your credentials to continue.</p>
+    </div>
         <div>
+            <EmailLogin />
+        </div>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LoginPage;
+export default NewLoginPage
